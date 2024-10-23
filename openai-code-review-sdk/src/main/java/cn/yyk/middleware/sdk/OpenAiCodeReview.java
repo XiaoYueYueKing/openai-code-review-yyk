ddca9c0f62a877281b46c1ca6ef0d08b37cd0576
yueyueking <945362994@qq.com>
2024-10-23 17:16:23 +0800

    feat: 检出代码

--- openai-code-review-sdk/src/main/java/cn/yyk/middleware/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/cn/yyk/middleware/sdk/OpenAiCodeReview.java
@@ -1,5 +1,9 @@
 package cn.yyk.middleware.sdk;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+
 /**
  * @BelongsProject: openai-code-review-yyk
  * @BelongsPackage: cn.yyk.middleware.sdk
@@ -9,7 +13,26 @@ package cn.yyk.middleware.sdk;
  * @Version: 1.0
  */
 public class OpenAiCodeReview {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("测试执行");
+
+        //1、代码检出
+        ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD~1", "HEAD");
+        processBuilder.directory(new File("."));
+
+        Process process = processBuilder.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+        String line;
+        StringBuilder diffCode = new StringBuilder();
+        while ((line = reader.readLine()) != null) {
+            diffCode.append(line);
+        }
+
+        int exitCode = process.waitFor();
+        System.out.println("Exited with code:" + exitCode);
+
+        System.out.println("评审代码：" + diffCode.toString());
     }
 }
--- openai-code-review-test/src/main/java/cn/yyk/middleware/Application.java
+++ openai-code-review-test/src/main/java/cn/yyk/middleware/Application.java
@@ -9,5 +9,9 @@ package cn.yyk.middleware;
  * @Version: 1.0
  */
 public class Application {
-
+    public static void main(String[] args) {
+        System.out.println("aaaa1");
+        System.out.println("aaaa2");
+        System.out.println("aaaa3");
+    }
 }
