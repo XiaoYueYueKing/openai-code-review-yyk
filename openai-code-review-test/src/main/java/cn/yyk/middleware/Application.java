14b69dc9dd767e8369c938174d44f51a52ef81b8
yueyueking <945362994@qq.com>
2024-10-24 17:30:23 +0800

    feat: 评审日志写入文件5.0

--- openai-code-review-test/src/main/java/cn/yyk/middleware/Application.java
+++ openai-code-review-test/src/main/java/cn/yyk/middleware/Application.java
@@ -10,6 +10,6 @@ package cn.yyk.middleware;
  */
 public class Application {
     public static void main(String[] args) {
-        System.out.println("aaaa12345");
+        System.out.println("aaaa123456");
     }
 }
